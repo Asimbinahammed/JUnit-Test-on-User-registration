144c0cc81b917cc6b0e6847512d40a584980eb6b
asimbinahammed <asim242370@gmail.com>
2021-09-16 23:47:41 +0530

    Last name validation

--- src/main/java/com/bridgelab/UserValidator.java
+++ src/main/java/com/bridgelab/UserValidator.java
@@ -7,6 +7,8 @@ public class UserValidator {
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}";
     //Last Name Regular Expression
     private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
+    //Email Address Regular Expression
+    private static final String EMAIL_ADDRESS_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";;
 
     //Checking boolean Value for First Name
     public boolean validateFirstName(String firstName) {
@@ -14,8 +16,15 @@ public class UserValidator {
         return pattern.matcher(firstName).matches();
     }
 
+    //Checking boolean Value for Last Name
     public boolean validateLastName(String lastName) {
         Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
         return pattern.matcher(lastName).matches();
+    }
+
+    //Checking boolean Value for Email Address
+    public boolean validateEmailAddress(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_ADDRESS_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
--- src/test/java/com/bridgelab/JUnitAssertionTest.java
+++ src/test/java/com/bridgelab/JUnitAssertionTest.java
@@ -3,9 +3,8 @@ package com.bridgelab;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class JUnitAssertionTest {
-    //Test Method
+    //Name Written in Proper way
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
@@ -13,10 +12,75 @@ public class JUnitAssertionTest {
         Assertions.assertEquals(true, result);
     }
 
+    //Name Written in LowerCase for Testing
     @Test
-    public void givenFirstName_WhenImroper_ShouldReturnFalse() {
+    public void givenFirstName_WhenFirsLetterSmall_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
         boolean result = validator.validateFirstName("asim");
-        Assertions.assertEquals(false, result);
+        Assertions.assertFalse(result);
+    }
+
+    //Name Written Less Than 3 Character
+    @Test
+    public void givenFirstName_WhenLessThanThreeCharacter_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("As");
+        Assertions.assertFalse(result);
+    }
+
+    //Testcase for special Characters
+    @Test
+    public void givenFirstName_WhenSpCharacters_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("as@#$");
+        Assertions.assertFalse(result);
+    }
+
+    //Last Name In Proper Way
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Ahammed");
+        Assertions.assertEquals(true, result);
+    }
+
+    //Test for Lowercase
+    @Test
+    public void givenLastName_WhenLowerCase_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("ahammed");
+        Assertions.assertFalse(result);
+    }
+
+    //Test for less than 3 character
+    @Test
+    public void givenLastName_WhenLessThanThreeCharacter_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("ah");
+        Assertions.assertFalse(result);
+    }
+
+    //Test for Special Character
+    @Test
+    public void givenLastName_WhenSpecialCharacter_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("ah@#$%");
+        Assertions.assertFalse(result);
+    }
+
+    //Test for Email Should Return True
+    @Test
+    void givenEmailAddress_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateEmailAddress("asim123@gmail.com");
+        Assertions.assertEquals(true, result);
+    }
+
+    //Test for invalid Email
+    @Test
+    void givenEmailAddress_WhenWrong_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateEmailAddress("abc@.com.my");
+        Assertions.assertFalse(result);
     }
 }
