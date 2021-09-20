c531103aaf23fad635fb5f3b7f33716b044847c5
asimbinahammed <asim242370@gmail.com>
2021-09-20 10:45:31 +0530

    Paametrised test to validate email entry

--- src/test/java/com/bridgelab/EmailSampleValidation.java
+++ src/test/java/com/bridgelab/EmailSampleValidation.java
@@ -2,71 +2,53 @@ package com.bridgelab;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class EmailSampleValidation {
+import java.util.Arrays;
+import java.util.Collection;
 
-    //testing all email samples
-    @Test
-    public void testingMail1() {
-        UserValidator validator = new UserValidator();
-        boolean result1 = validator.validateEmailAddress("abc@yahoo.com");
-        Assertions.assertEquals(true, result1);//Test will be pass
+@RunWith(Parameterized.class)
+public class ValidEmailTest {
+    private String email2Test;
+    private boolean expectedResult;
+
+    public ValidEmailTest(String email, boolean expectedResult) {
+        this.email2Test = email;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true},
+                {"abc@1.com", true},
+                {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true},
+                {"abc", false},
+                {" abc@.com.my ", false},
+                {"abc123@gmail.a", false},
+                {"abc123@.com", false},
+                {"abc123@.com.com", false},
+                {".abc@abc.com ", false},
+                {"abc()*@gmail.com", false},
+                {"abc@%*.com", false},
+                {"abc..2002@gmail.com ", false},
+                {"abc.@gmail.com", false},
+                {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}});
     }
 
     @Test
-    public void testingMail2() {
+    public void givenEmailAsVar_ShouldReturnAsPerTheParameterizedResult() {
         UserValidator validator = new UserValidator();
-        boolean result2 = validator.validateEmailAddress("abc-100@yahoo.com");
-        Assertions.assertEquals(true, result2);//Test will be pass
-    }
-
-    @Test
-    public void testingMail3() {
-        UserValidator validator = new UserValidator();
-        boolean result3 = validator.validateEmailAddress("abc111@abc.com");
-        Assertions.assertEquals(true, result3);//Test will be pass
-    }
-
-    @Test
-    public void testingMail4() {
-        UserValidator validator = new UserValidator();
-        boolean result4 = validator.validateEmailAddress("abc-100@abc.net");
-        Assertions.assertEquals(true, result4);//Test will be pass
-    }
-
-    @Test
-    public void testingMail5() {
-        UserValidator validator = new UserValidator();
-        boolean result5 = validator.validateEmailAddress("abc.100@abc.com.au");
-        Assertions.assertEquals(true, result5);//Test will be pass
-    }
-
-    @Test
-    public void testingMail6() {
-        UserValidator validator = new UserValidator();
-        boolean result6 = validator.validateEmailAddress("abc@1.com");
-        Assertions.assertEquals(true, result6);//Test will be pass
-    }
-
-    @Test
-    public void testingMail7() {
-        UserValidator validator = new UserValidator();
-        boolean result7 = validator.validateEmailAddress("abc@gmail.com.com");
-        Assertions.assertEquals(true, result7);//Test will be pass
-    }
-
-    @Test
-    public void testingMail8() {
-        UserValidator validator = new UserValidator();
-        boolean result8 = validator.validateEmailAddress("abc");
-        Assertions.assertEquals(true, result8);//Test will be fail
-    }
-
-    @Test
-    public void testingMail9() {
-        UserValidator validator = new UserValidator();
-        boolean result9 = validator.validateEmailAddress("abc@.com.my");
-        Assertions.assertEquals(true, result9);//Test will be fail
+        boolean result = validator.validateEmailAddress(this.email2Test);
+        Assertions.assertEquals(this.expectedResult, result);
     }
 
 }
